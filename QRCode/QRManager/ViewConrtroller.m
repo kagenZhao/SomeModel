8cc05d4b0b3f8dc060fe64d674893574592ee669
kagenMonster <1149467687@qq.com>
2016-03-28 20:58:16 +0800

    no message

--- QRCode/QRManager/ViewConrtroller.m
+++ QRCode/QRManager/ViewConrtroller.m
@@ -19,8 +19,9 @@
 @implementation ViewConrtroller
 - (IBAction)create:(id)sender {
   
-  
+  // 用CIFilter
   UIImage *img = [QRManager createQRCodeImageWithMessage:@"1" size:_qrimage.frame.size.width color:[UIColor redColor] topImg:[UIImage imageNamed:@"2"]];
+  // 用libqrencode
   UIImage *image = [QRCodeGenerator qrImageForString:@"1" imageSize:_qrimage.frame.size.width Topimg:[UIImage imageNamed:@"2"]];
   _qrimage.image = img;
 }

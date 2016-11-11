33085e34411172b7da287f0baeab40a0ec7f9dc7
kagenZhao <1149467687@qq.com>
2016-11-11 18:25:54 +0800

    fix optional bug

--- KZQRManager/KZQRManager/KZQREncode.swift
+++ KZQRManager/KZQRManager/KZQREncode.swift
@@ -11,9 +11,8 @@ import UIKit
 public extension KZQRManager where Type: KZQREncodeProtocol {
     func encodeQR(to size: CGFloat = 200.0) -> UIImage? {
         guard let ciimage = createCiimage() else { return nil }
-        var resultImage = createUIImage(from: ciimage, size: size)
-        resultImage = transparent(with: resultImage!, to: CIColor(color: UIColor.red))
-        return resultImage
+        guard let defaultImage = createUIImage(from: ciimage, size: size) else { return nil }
+        return transparent(with: defaultImage, to: CIColor(color: UIColor.red))
     }
     
     private func createCiimage() -> CIImage? {
@@ -46,9 +45,9 @@ public extension KZQRManager where Type: KZQREncodeProtocol {
         let bytePerRow = w * 4
         let rgbimageBuf: UnsafeMutablePointer<UInt32> = malloc(bytePerRow * h).bindMemory(to: UInt32.self, capacity: 1)
         let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext.init(data: rgbimageBuf, width: w, height: h, bitsPerComponent: 8, bytesPerRow: bytePerRow, space: colorSpace, bitmapInfo: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.noneSkipLast.rawValue)
-        guard let cgimage = image.cgImage, context != nil else { return nil }
-        context!.draw(cgimage, in: CGRect(origin: CGPoint.zero, size: CGSize(width: w, height: h)))
+        guard let context = CGContext.init(data: rgbimageBuf, width: w, height: h, bitsPerComponent: 8, bytesPerRow: bytePerRow, space: colorSpace, bitmapInfo: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.noneSkipLast.rawValue) else { return nil }
+        guard let cgimage = image.cgImage else { return nil }
+        context.draw(cgimage, in: CGRect(origin: CGPoint.zero, size: CGSize(width: w, height: h)))
         for i in 0..<(w * h) {
            let pCurPtr = rgbimageBuf.advanced(by: i)
             if (pCurPtr.pointee & 0xffffff00) < 0x99999900 {
--- KZQRManager/KZQRManager/ViewController.swift
+++ KZQRManager/KZQRManager/ViewController.swift
@@ -15,12 +15,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var iiimage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.kqr.setupQRUIInSelf()?
-        //.setPreviewLayerFrameInSuperLayer(CGRect(x: 100, y: 100, width: 100, height: 100))
-        //.setOutputRectOfInterest(CGRect(x: 0.2, y: 0.3, width: 0.5, height: 0.5))
-//        .startRunning { (str) in
-//            print(str)
-//        }
+        
+        self.kqr.setupQRUIInSelf()?
+        .setPreviewLayerFrameInSuperLayer(CGRect(x: 100, y: 100, width: 100, height: 100))
+        .setOutputRectOfInterest(CGRect(x: 0.2, y: 0.3, width: 0.5, height: 0.5))
+        .startRunning { (str) in
+            print(str)
+        }
         
         let image = "sssssssssss".kqr.encodeQR()!
         PHPhotoLibrary.shared().performChanges({

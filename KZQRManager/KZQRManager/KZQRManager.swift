4e696841fafbd81d8568a5da49da95f955838f60
kagenZhao <1149467687@qq.com>
2016-11-21 16:05:29 +0800

    no message

--- KZQRManager/KZQRManager/KZQRManager.swift
+++ KZQRManager/KZQRManager/KZQRManager.swift
@@ -9,19 +9,26 @@
 import UIKit
 
 public final class KZQRManager<Type> {
+    
     let base: Type
+    
     init(_ base: Type) {
+        
         self.base = base
     }
 }
 
 public protocol KZQRManagerCompatible {
+    
     associatedtype CompatibleType
+    
     var kqr: CompatibleType { get }
 }
 
 public extension KZQRManagerCompatible {
+    
     public var kqr: KZQRManager<Self> {
+        
         get { return KZQRManager(self) }
     }
     
@@ -29,45 +36,55 @@ public extension KZQRManagerCompatible {
 
 /// you can set another Class to conform this protocol
 public protocol KZQRDecodeUIProtocol: KZQRManagerCompatible, NSObjectProtocol {
+    
     var decodeUISuperLayer: CALayer { get }
 }
 
 /// you can set another Class to conform this protocol
 public protocol KZQRDecodeProtocol: KZQRManagerCompatible {
+    
     var decodeImage: UIImage { get }
 }
 
 /// you can set another Class to conform this protocol
 public protocol KZQREncodeProtocol: KZQRManagerCompatible {
+    
     var encodeMessage:String { get }
 }
 
 
 extension UIImage: KZQRDecodeProtocol {
+    
     public var decodeImage: UIImage { return self }
 }
 
 extension String: KZQREncodeProtocol {
+    
     public var encodeMessage: String { return self }
 }
 
 extension NSString: KZQREncodeProtocol {
+    
     public var encodeMessage: String { return self as String }
 }
 
 extension URL: KZQREncodeProtocol {
+    
     public var encodeMessage: String { return self.absoluteString }
 }
 
 extension NSURL: KZQREncodeProtocol {
+    
     public var encodeMessage: String { return self.absoluteString ?? "" }
 }
 
 extension UIView: KZQRDecodeUIProtocol {
+    
     public var decodeUISuperLayer: CALayer { return self.layer }
 }
 
 extension UIViewController: KZQRDecodeUIProtocol {
+    
     public var decodeUISuperLayer: CALayer { return self.view.layer }
 }
 
